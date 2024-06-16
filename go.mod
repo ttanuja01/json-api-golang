b747e1f2d6e63e574bb9d7d97304805bda7588c6
ttanuja <tanujathirupathi1901@gmail.com>
2024-06-16 23:51:31 +0530

    feat(login):added login endpoint

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.3
 
 require (
 	github.com/golang-jwt/jwt/v4 v4.4.2
-	github.com/gorilla/mux v1.8.1 // used for http request routing
-	github.com/lib/pq v1.10.9
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.7
+	golang.org/x/crypto v0.24.0
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 )
@@ -11,8 +12,29 @@ func NewAPIServer(listenAddr string, store Storage) *APIServer {
 		store:      store,
 	}
 }
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account => ", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "anthony", "GG", "hunter88888")
+}
 
 func main() {
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
@@ -22,7 +44,11 @@ func main() {
 		log.Fatal(err)
 	}
 
+	if *seed {
+		fmt.Println("seeding the database")
+		seedAccounts(store)
+	}
+
 	server := NewAPIServer(":3000", store)
 	server.Run()
-	fmt.Println("Hi Buddy!!, server is running")
 }
